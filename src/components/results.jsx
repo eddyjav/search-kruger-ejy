a45146e2eea487818b9372a3021b3297595184a8
eddyjav <eddy.j.av@gmail.com>
2023-01-07 16:52:38 -0500

    :mag_right: add component/results

---
+++ src/components/results.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState, useMemo, useEffect } from "react";
+import MarkedItem from "./markedItem";
+
+const Results = ({ items, onItemSelected, query, onResultsCalculated }) => {
+  const [results, setResults] = useState([]);
+
+  const filterItems = useMemo(() => findMatch(items, query), [items, query]);
+
+  useEffect(() => {
+    onResultsCalculated(results);
+  }, [results]);
+
+  function findMatch(items, query) {
+    const res = items.filter((i) => {
+      return i.title.toLowerCase().indexOf(query) >= 0 && query.length > 0;
+    });
+
+    setResults(res);
+    return res;
+  }
+
+  return (
+    <div>
+      {query !== ""
+        ? filterItems.map((item) => (
+            <MarkedItem
+              key={item.id}
+              item={item}
+              onClick={onItemSelected}
+              query={query}
+            />
+          ))
+        : ""}
+    </div>
+  );
+};
+
+export default Results;
