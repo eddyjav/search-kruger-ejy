1f012979ab4afa3c7c1eca44f41cc0f767265eea
eddyjav <eddy.j.av@gmail.com>
2023-01-07 16:55:54 -0500

    :mag_right: add component/markedItem & modified finals results

---
+++ src/components/markedItem.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useMemo } from "react";
+
+const MarkedItem = ({ item, query, onClick }) => {
+  const { left, center, right } = useMemo(
+    () => getPositions(item, query),
+    [item, query]
+  );
+
+  function getPositions(item, query) {
+    const index = item.title.toLowerCase().indexOf(query);
+    const left = item.title.slice(0, index);
+    const right = item.title.slice(index + query.length);
+    const center = item.title.slice(index, index + query.length);
+
+    return {
+      left,
+      center,
+      right,
+    };
+  }
+
+  const handleClick = () => {
+    onClick(item);
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {left}
+      <span style={{ fontWeight: "bolder", backgroundColor: "yellow" }}>
+        {center}
+      </span>
+      {right}
+    </button>
+  );
+};
+
+export default MarkedItem;
